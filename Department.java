373b785b30a45013be70255bb2ac82a3627b37df
learner0116-sys <learner0116@gmail.com>
2026-09-12 02:57:01 +0300

    Refactor Department class with improved comments
    Removed unnecessary comments and added clarifying comments for fields and methods.

--- Department.java
+++ Department.java
@@ -9,20 +9,18 @@ import java.util.List;
  *
  * @author WIN(10)X64
  */
-
-
 public class Department {
-
+     // fields 
     private String name;
     private List<Course> courses = new ArrayList<>();
     private List<Student> students = new ArrayList<>();
-
+   //constructor
     public Department(String name) {
         this.name = name;
     }
-
+    //getter 
     public String getName() { return name; }
-
+   //methods to add courses& students
     public void addCourse(Course course) {
         courses.add(course);
     }
@@ -30,10 +28,10 @@ public class Department {
     public void addStudent(Student student) {
         students.add(student);
     }
-
+    // fields needed to be added before making getters of it
     public List<Course> getCourses() { return courses; }
     public List<Student> getStudents() { return students; }
-
+  //method
     public void displayAllCourses() {
         System.out.println("\n===== Courses in \"" + name + "\" Department =====");
         for (Course c : courses) {
