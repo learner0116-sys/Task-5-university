2a66f2d3ace20d6def00ea731c16b732d7de545f
learner0116-sys <learner0116@gmail.com>
2026-09-12 00:05:46 +0300

    Add files via upload

---
+++ University.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.university;
+
+/**
+ *
+ * @author WIN(10)X64
+ */
+public class University {
+
+    public static void main(String[] args) {
+
+        Department csDept = new Department("Computer Science");
+
+        TheoryCourse algorithms = new TheoryCourse("Algorithms", 20);
+        LabCourse programmingLab = new LabCourse("Programming Lab", "Building A - Room 101");
+
+        csDept.addCourse(algorithms);
+        csDept.addCourse(programmingLab);
+
+        Student student1 = new Student("Ahmed");
+        Student student2 = new Student("Sara");
+
+        csDept.addStudent(student1);
+        csDept.addStudent(student2);
+
+        student1.enroll(algorithms);
+        student2.enroll(programmingLab);
+        student2.enroll(algorithms);
+
+        csDept.displayAllCourses();
+
+        System.out.println();
+        student1.displayCourses();
+        System.out.println();
+        student2.displayCourses();
+    }
+
+}
