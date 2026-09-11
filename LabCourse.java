a5da92768155169f91973f236ff1a28aa8e990e9
learner0116-sys <learner0116@gmail.com>
2026-09-12 02:49:37 +0300

    Refactor LabCourse.java with improved comments
    Removed unnecessary comments and added clarifying comments.

--- LabCourse.java
+++ LabCourse.java
@@ -9,13 +9,15 @@ package com.mycompany.university;
  * @author WIN(10)X64
  */
 public class LabCourse extends Course {
-
+    //inheritance ==> extend
+    //attribute
     private String labLocation;
-
+  //constructor 
     public LabCourse(String name, String labLocation) {
-        super(name);
+        super(name);   //inhertance ==>super
         this.labLocation = labLocation;
     }
+    //getter/setter
 
     public String getLabLocation() { return labLocation; }
     public void setLabLocation(String labLocation) { this.labLocation = labLocation; }
