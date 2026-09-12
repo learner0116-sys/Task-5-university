dc42ae62b058e1f914c3d62e62651a6ea314e04d
learner0116-sys <learner0116@gmail.com>
2026-09-12 03:09:17 +0300

    Enhance University.java with explanatory comments
    Added comments to clarify object creation and method calls.

--- University.java
+++ University.java
@@ -11,18 +11,18 @@ package com.mycompany.university;
 public class University {
 
     public static void main(String[] args) {
-
+           //create object / instance 
         Department csDept = new Department("Computer Science");
 
         TheoryCourse algorithms = new TheoryCourse("Algorithms", 20);
         LabCourse programmingLab = new LabCourse("Programming Lab", "Building A - Room 101");
-
+       //calling methods from another class
         csDept.addCourse(algorithms);
         csDept.addCourse(programmingLab);
 
         Student student1 = new Student("Ahmed");
         Student student2 = new Student("Sara");
-
+//calling methods from another class
         csDept.addStudent(student1);
         csDept.addStudent(student2);
 
